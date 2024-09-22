d699c2b179837df5ffaae537e35e2f761e719908
曹子良 <ziliang.cao@qunar.com>
2024-09-22 21:59:23 +0800

    滑动窗口

---
+++ src/main/java/cn/czl/algorithm/day_20240922/Solution.java
@@ -0,0 +1,46 @@
+package cn.czl.algorithm.day_20240922;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public static void main(String[] args) {
+        new Solution().findAnagrams("abab","ab");
+    }
+    public List<Integer> findAnagrams(String s, String p) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (char c : p.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        List<Integer> res = new ArrayList<>();
+        int left = 0, right = 0;
+        int valid = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            if (need.containsKey(c)){
+                window.put(c,window.getOrDefault(c,0)+1);
+                if (window.get(c).equals(need.get(c))){
+                    valid++;
+                }
+            }
+            if (right-left >= p.length()){
+                if (valid == need.size()){
+                    res.add(left);
+                }
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)){
+                    if (window.get(d).equals(need.get(d))){
+                        valid--;
+                    }
+                    window.put(d,window.get(d)-1);
+                }
+            }
+        }
+        return res;
+    }
+}
